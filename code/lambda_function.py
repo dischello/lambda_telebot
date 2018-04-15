df7348ec78d63fdef7ac2afdcf805920c25fa191
Rezo Heorhadze <rezo_heorhadze@epam.com>
2018-04-16 00:05:47 +0300

    Update python script

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -1,15 +1,16 @@
 import requests
+import os
+
+token = os.environ["APIStage"]
+tokens = {'dev': "504709962:AAEb2zGgMy2rBMrMnwBIBTtkjAHhKRC7XS8",'stage':"", '550333385:AAGrL-1HQFvgmemw42jRGsoZkO-lHTQTcK0': "563678020:AAGPx21beePzxBqRFQAnQqEb0gA_LURyfho"}
 
 def lambda_handler(event, context):
-
-    data=event
-    print(data.json())
-    send_message(data["message"]["from"]["id"], data["message"]["text"])
+    send_message(event["message"]["from"]["id"], event["message"]["text"])
 
 def send_message(chat_id, text):
     url = "https://api.telegram.org/bot{token}/{method}".format(
-        token="563678020:AAGPx21beePzxBqRFQAnQqEb0gA_LURyfho",
-        method="sendMessage"
+        token = tokens[token],
+        method = "sendMessage"
     )
     data = {
         "chat_id": chat_id,
